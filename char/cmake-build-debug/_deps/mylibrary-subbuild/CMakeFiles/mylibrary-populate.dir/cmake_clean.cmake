2e403ccfd326ac2a6f1d8f74e1f12dd82a87d055
Victor Delaplaine <victor.delaplaine@hpe.com>
2024-03-03 20:21:53 -0800

    trying to get cmake

---
+++ char/cmake-build-debug/_deps/mylibrary-subbuild/CMakeFiles/mylibrary-populate.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mylibrary-populate"
+  "CMakeFiles/mylibrary-populate-complete"
+  "mylibrary-populate-prefix/src/mylibrary-populate-stamp/mylibrary-populate-build"
+  "mylibrary-populate-prefix/src/mylibrary-populate-stamp/mylibrary-populate-configure"
+  "mylibrary-populate-prefix/src/mylibrary-populate-stamp/mylibrary-populate-download"
+  "mylibrary-populate-prefix/src/mylibrary-populate-stamp/mylibrary-populate-install"
+  "mylibrary-populate-prefix/src/mylibrary-populate-stamp/mylibrary-populate-mkdir"
+  "mylibrary-populate-prefix/src/mylibrary-populate-stamp/mylibrary-populate-patch"
+  "mylibrary-populate-prefix/src/mylibrary-populate-stamp/mylibrary-populate-test"
+  "mylibrary-populate-prefix/src/mylibrary-populate-stamp/mylibrary-populate-update"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/mylibrary-populate.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ char/cmake-build-debug/_deps/mylibrary-subbuild/CMakeFiles/mylibrary-populate.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty custom commands generated dependencies file for mylibrary-populate.
+# This may be replaced when dependencies are built.
